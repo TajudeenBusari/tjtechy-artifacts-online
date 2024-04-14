82abf112831f1baa3f5d4a5931af62bf64cca545
tajud <tajud@TAJUDEEN>
2024-04-14 22:46:58 +0300

    this is the first commit.

---
+++ src/main/java/com/tjtechy/artifactsOnline/TjtechyArtifactsOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.tjtechy.artifactsOnline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TjtechyArtifactsOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TjtechyArtifactsOnlineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tjtechy/artifactsOnline/TjtechyArtifactsOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tjtechy.artifactsOnline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TjtechyArtifactsOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
