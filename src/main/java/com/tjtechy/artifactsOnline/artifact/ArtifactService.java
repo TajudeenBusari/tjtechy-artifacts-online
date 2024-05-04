786ace4c7d0cfc8e5e70c55dcc1e3d2cce4ab093
tajud <tajud@TAJUDEEN>
2024-05-04 18:57:58 +0300

    Add observability stack(Prometheus, Grafana and Zipkin) to the project.

--- src/main/java/com/tjtechy/artifactsOnline/artifact/ArtifactService.java
+++ src/main/java/com/tjtechy/artifactsOnline/artifact/ArtifactService.java
@@ -3,6 +3,7 @@ package com.tjtechy.artifactsOnline.artifact;
 import com.tjtechy.artifactsOnline.artifact.utils.IdWorker;
 import com.tjtechy.artifactsOnline.system.exception.ObjectNotFoundException;
 import io.micrometer.core.annotation.Timed;
+import io.micrometer.observation.annotation.Observed;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
@@ -24,6 +25,7 @@ public class ArtifactService {
     this.idWorker = idWorker;
   }
 
+  @Observed(name = "artifact", contextualName = "findByIdService")
   public Artifact findById(String artifactId){
 
     return this.artifactRepository.findById(artifactId)
